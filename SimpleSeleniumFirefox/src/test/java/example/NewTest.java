5c9f57f6d532fcdc6c566c0a9aa5e9a8a6bb9030
vdonkor <vincent.donkor@gmail.com>
2019-05-01 18:27:43 -0400

    testing firefox

---
+++ SimpleSeleniumFirefox/src/test/java/example/NewTest.java
@@ -0,0 +1,43 @@
+package example;
+
+
+//import org.openqa.selenium.By;	
+//import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;		
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.testng.Assert;		
+import org.testng.annotations.Test;	
+import org.testng.annotations.BeforeTest;	
+import org.testng.annotations.AfterTest;	
+
+public class NewTest {
+	
+  public WebDriver driver;
+  String driverPath = "/usr/local/bin/geckodriver";
+  
+  @Test
+  public void firstTest() {
+	  
+	  driver.get("http://demo.guru99.com/test/guru99home/");  
+	  String title = driver.getTitle();				 
+	  Assert.assertTrue(title.contains("Demo Guru99 Page"));
+	  
+//	  //Search on Google
+//	  driver.findElement(By.name("q")).sendKeys("selenium webdriver");
+//	  driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+  }
+  @BeforeTest
+  public void beforeTest() {
+	  System.setProperty("webdriver.gecko.driver", driverPath);
+	  FirefoxOptions options = new FirefoxOptions();
+	  options.setHeadless(true);
+      driver = new FirefoxDriver();
+  }
+
+  @AfterTest
+  public void afterTest() {
+	  driver.close();	
+  }
+
+}
