eff81d258406c47928716a7c580c88eeeb61bdc7
vdonkor <vincent.donkor@gmail.com>
2019-05-02 01:54:13 -0400

    Testing headless

--- SimpleSeleniumFirefox/src/test/java/example/NewTest.java
+++ SimpleSeleniumFirefox/src/test/java/example/NewTest.java
@@ -1,31 +1,41 @@
 package example;
 
 
-//import org.openqa.selenium.By;	
-//import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;		
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
-import org.testng.Assert;		
-import org.testng.annotations.Test;	
-import org.testng.annotations.BeforeTest;	
-import org.testng.annotations.AfterTest;	
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
 
 public class NewTest {
-	
+
   public WebDriver driver;
   String driverPath = "/bin/geckodriver";
-  
+
   @Test
   public void firstTest() {
-	  
-	  driver.get("http://demo.guru99.com/test/guru99home/");  
-	  String title = driver.getTitle();				 
+
+	  driver.get("http://demo.guru99.com/test/guru99home/");
+	  String title = driver.getTitle();
 	  Assert.assertTrue(title.contains("Demo Guru99 Page"));
-	  
-//	  //Search on Google
-//	  driver.findElement(By.name("q")).sendKeys("selenium webdriver");
-//	  driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+
+		//Search on Google
+		driver.findElement(By.name("q")).sendKeys("selenium webdriver");
+		driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		//Display number of results on Console
+		System.out.println("Total Results - " + driver.findElement(By.id("resultStats")).getText());
+
+
   }
   @BeforeTest
   public void beforeTest() {
@@ -37,7 +47,7 @@ public class NewTest {
 
   @AfterTest
   public void afterTest() {
-	  driver.close();	
+	  driver.close();
   }
 
 }
