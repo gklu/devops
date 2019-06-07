a02901a8ca098f887d2cccdb3f97f56ca53ac52f
vdonkor <vincent.donkor@gmail.com>
2019-06-06 23:31:08 -0400

    Changed pipeline

---
+++ jobs/icdc/pipeline.groovy
@@ -0,0 +1,19 @@
+pipelineJob('icdc/c9c') {
+
+  def repo = 'https://github.com/gklu/devops.git' 
+  description("ICDC pipeline Job") 
+
+  definition  {
+    cpsScm {
+      scm {
+        git {
+          remote { url(repo) }
+          branches('master', '**/feature*')
+          scriptPath('jobs/icdc/Jenkinsfile')
+          extensions { }  // required to avoid tagging
+        }
+      }
+      sandbox()
+    }
+  }
+}
