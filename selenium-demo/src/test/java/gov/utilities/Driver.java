63e4f61829beade1221f0bee5c865c20ddaf89e0
vdonkor <vincent.donkor@gmail.com>
2019-08-11 02:56:36 -0400

    removed headless option

--- selenium-demo/src/test/java/gov/utilities/Driver.java
+++ selenium-demo/src/test/java/gov/utilities/Driver.java
@@ -24,14 +24,14 @@ public  static WebDriver driver;
 			break;
 
 			case "chrome":
-				// ChromeOptions options = new ChromeOptions();
+				ChromeOptions options = new ChromeOptions();
 		        // options.addArguments( "--headless", "--disable-gpu", "--no-sandbox","--disable-dev-shm-usage'","--whitelisted-ips");
-				// options.addArguments( "--disable-gpu","--disable-dev-shm-usage'","--whitelisted-ips");
+				options.addArguments( "--disable-gpu","--disable-dev-shm-usage'","--whitelisted-ips");
 
 		      
 				System.setProperty("webdriver.chrome.driver", Config_Reader.getProperty("chrome.driver.path"));
-				//   setDriver(new ChromeDriver(options));
-				setDriver(new ChromeDriver());
+				  setDriver(new ChromeDriver(options));
+				// setDriver(new ChromeDriver());
 
 			break;
 			
